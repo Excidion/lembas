dae5974c2330a8cdd3d3e86b9d898e14320f90cb
Excidion <36995046+Excidion@users.noreply.github.com>
2022-11-20 13:42:44 +0100

    simple implementation of detecting new offers

--- appclient.py
+++ appclient.py
@@ -3,6 +3,10 @@ from tgtg import TgtgClient, TgtgAPIError
 
 class AppClient(TgtgClient):
 
+    def __init__(self,*args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.memory = dict()
+
     @property
     def registration_email(self):
         try:
@@ -28,12 +32,18 @@ class AppClient(TgtgClient):
             if (item.get("items_available") > 0) or show_unavailable:
                 yield make_item_text(item)
 
-    def get_new_items_texts(self):
+    def get_new_item_texts(self):
         for item in self.get_new_items():
             yield make_item_text(item)
 
     def get_new_items(self):
-        pass # TODO
+        for item in self.get_items():
+            item_id = item.get("item").get("item_id")
+            items_available = item.get("items_available")
+            items_available_before = self.memory.setdefault(item_id, 0)
+            if items_available > items_available_before:
+                self.memory[item_id] = items_available
+                yield item
 
 
 def make_item_text(item):
