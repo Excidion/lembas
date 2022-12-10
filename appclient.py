b67c4f4a2adc13d530c25e79e619c3a586895008
Excidion <36995046+Excidion@users.noreply.github.com>
2022-12-10 14:39:04 +0100

    excape special character in link

--- appclient.py
+++ appclient.py
@@ -61,5 +61,5 @@ def make_item_text(item):
     msg0 = f"*{store_name}* has {items_available} {item_name or item_category} available for {item_price:.2f} {currency} each."
     msg1 = f"You can pick it up from {pickup_start.time().strftime('%H:%M')} to {pickup_end.time().strftime('%H:%M')} {on}at {store_address}."
     item_id = item["item"]["item_id"]
-    link = f"[Grab it!](https://share.toogoodtogo.com/item/{item_id}/)"
+    link = f"[Grab it\!](https://share.toogoodtogo.com/item/{item_id}/)"
     return f"{msg0}\n{msg1}\n{link}"
